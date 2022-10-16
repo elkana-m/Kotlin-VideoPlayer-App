936350d86fa06a1fd27b989d06337018b27386db
elkana-m <munganga.elkana@yahoo.fr>
2022-10-15 22:42:44 -0500

    moved media controller from MainActivity to SecondFragment - User can't play yet

--- app/src/main/java/com/example/vplay/SecondFragment.kt
+++ app/src/main/java/com/example/vplay/SecondFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.vplay.databinding.FragmentSecondBinding
+import android.net.Uri
+import android.net.Uri.*
+import android.widget.VideoView
+import android.widget.MediaController
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,7 +18,7 @@ import com.example.vplay.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-
+    private lateinit var videoView: VideoView
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -31,6 +35,19 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        videoView = findViewById(R.id.testView)
+        videoView = requireView().findViewById(R.id.testView)
+//        val mediaController = MediaController(this)
+//        mediaController.setAnchorView(videoView)
+        // specify location of media file
+//        val uri: Uri = parse("android.resource://" + packageName + "/" + "raw/test")
+//        val uri: Uri = parse("android.resource://" + packageName + "/" + "raw/test")
+//        // setting media controller & URI, then starting the videoView
+//        videoView.setMediaController(mediaController)
+//        videoView.setVideoURI(uri)
+//        videoView.requestFocus()
+//        videoView.start()
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
